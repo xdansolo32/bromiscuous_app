106ae68e63f6a301dda5f0cf2d49c23bf6eeb71a
xdansolo32 <xdansolo32@hotmail.com>
2011-11-14 18:46:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BromiscuousApp::Application.config.secret_token = 'c59c00c27d379339cc28ec925e19bdb8d2ab416a30583d50ae854885a7e58ae64ddc2ff47f24ab501020156a1e6ef567b4c2197df80afced7639fe76dfb56f4d'
