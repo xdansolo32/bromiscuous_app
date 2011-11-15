989e69dafdd5b40e6639028fddb8dc469ed1ef80
Daniel Sun <xdansolo32@hotmail.com>
2011-11-14 22:34:47 -0800

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "ET Phone Bro"
+  end
+  
+  def about
+    @title = "Bro Lifestyle"
   end
 
 end
